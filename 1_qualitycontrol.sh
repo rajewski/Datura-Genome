8e0db2e2b698e1f76a9fbfde6107c80d7d945621
rajewski <rajewski23@gmail.com>
2019-05-08 12:53:31 -0700

    add adpator fasta file

--- 1_qualitycontrol.sh
+++ 1_qualitycontrol.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l
 #SBATCH --ntasks=8
 #SBATCH --nodes=1
-#SBATCH --mem=100G
+#SBATCH --mem=10G
 #SBATCH --time=20:00:00
 #SBATCH --mail-user=araje002@ucr.edu
 #SBATCH --mail-type=ALL
-#SBATCH -p highmem
+#SBATCH -p batch
 #SBATCH -o history/slurm-%A.out
 set -euv
 
@@ -17,7 +17,7 @@ set -euv
 if [ ! -d "QualityControl" ]; then
     echo $(date) ": Running fastqc."
     module load fastqc/0.11.7
-    fastqc -t $SLURM_NTASKS -noextract -o QualityControl DNA-seq/*.fastq.gz
+    fastqc -t $SLURM_NTASKS -noextract -o 1_QCQA DNA-seq/*.fastq.gz
     echo $(date) ": Done."
 else
     echo $(date) ": fastqc has already been run. Let's trim and filter these reads."
@@ -28,12 +28,7 @@ fi
 module load trimmomatic/0.36
 
 #I'll add in an if statement once I know exactly how the outputs are structured so that I can check for them accurately.
-#make the output directory if necessary
-if [ ! -d "DNA-seqTrimmed" ]; then
-    mkdir DNA-seqTrimmed
-fi
-
-trimmomatic PE -threads $SLURM_NTASKS DNA-seq/Alex-1_S9_R1_001.fastq.gz DNA-seq/Alex-2_S10_R1_001.fastq.gz -baseout DNA-seqTrimmed/Dstr1Trim.fq.gz ILLUMINACLIP:TruSeq3-PE-2:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+trimmomatic PE -threads $SLURM_NTASKS DNA-seq/Alex-1_S9_R1_001.fastq.gz DNA-seq/Alex-2_S10_R1_001.fastq.gz -baseout DNA-seqTrimmed/Dstr1Trim.fq.gz ILLUMINACLIP:TruSeq3-PE-2.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
 #trimmomatic PE -threads $SLURM_NTASKS DNA-seq/Alex_S230_L002_R1_001.fastq.gz DNA-seq/Alex_S230_L002_R2_001.fastq.gz -baseout DNA-seqTrimmed/Dstr2Trim.fq.gz
 #trimmomatic PE -threads $SLURM_NTASKS DNA-seq/Alex_S85_L002_R1_001.fastq.gz DNA-seq/Alex_S85_L002_R2_001.fastq.gz -baseout DNA-seqTrimmed/Dstr3Trim.fq.gz
 
