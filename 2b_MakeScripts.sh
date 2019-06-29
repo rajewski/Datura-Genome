71dac82dab35841d90d95c9c9828ab629d27b2db
rajewski <rajewski23@gmail.com>
2019-06-29 09:15:38 -0700

    add email to submussion

--- 2b_MakeScripts.sh
+++ 2b_MakeScripts.sh
@@ -1,4 +1,4 @@
 module load  MaSuRCA/3.3.1
 masurca -o 2c_HybAssemble.sh 2a_HybAssembleConfig.txt
 
-sbatch --ntasks 32 -p intel --mem 256gb --out ./history/2c_HybAssemble-%A.out 2c_HybAssemble.sh
+sbatch --ntasks 32 -p intel --mem 256gb --out ./history/2c_HybAssemble-%A.out --mail-user=araje002@ucr.edu --mail-type=ALL 2c_HybAssemble.sh
