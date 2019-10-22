02b72163ec20e30eb520051fe5945a8cc20e9334
rajewski <rajewski23@gmail.com>
2019-10-22 14:26:01 -0700

    clean up genomesize

--- 2_Assemble/2e_HybAssemble2.sh
+++
@@ -1,3 +0,0 @@
-#After the megareads step has completed (2d_megareads1.sh), I rerun the assembly script
-#this is the submission to rerun the assembly script 
-sbatch --ntasks 32 -p highmem --mem 512gb --out ../history/2e_HybAssemble2-%A.out --mail-user=araje002@ucr.edu --mail-type=ALL 2c_HybAssemble.sh
