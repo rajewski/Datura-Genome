f650b4d24dc8c6cfb889966d84333b84539461a6
rajewski <rajewski23@gmail.com>
2019-11-05 08:20:25 -0800

    links more mem and fast-sg more automation

--- 3_Fast-SG/3_boxplot.R
+++ 3_Fast-SG/3_boxplot.R
@@ -1,11 +1,18 @@
 files=read.table(file="files-sort.txt")
+#Get insert expected sizes
+inserts <- strsplit(as.character(read.delim("ultra-long-conf.txt", header=F, sep=" ")$V4),",")[[1]]
+
+#Plot insert sizes
 pdf(file="Insert-Dstr.pdf")
 par(mfrow=c(5,4),mar=c(1, 4, 2, 1))
-b<-c("2kb","4kb","6kb","8kb","10kb","12kb","14kb",
-     "16kb","18kb","20kb","30kb","40kb","50kb","60kb","70kb",
-     "80kb","100kb","120kb","150kb","180kb")
+b<-inserts
 for(i in 1:length(files$V1)){
   b[i]=read.table(file=as.vector(files$V1[i]))
-  boxplot(b[[i]],outline=F,ylab="Insert size (kb)",las=2,boxwex=0.3)
-}
+  boxplot(b[[i]],
+          outline=F,
+          ylab="Insert size (kb)",
+          las=2,
+          boxwex=0.3, 
+          main=inserts[i])
+  }
 dev.off()
--- 3_Fast-SG/5_ScaffMatch.sh
+++ 3_Fast-SG/5_ScaffMatch.sh
@@ -2,25 +2,26 @@
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --mem=101G
-#SBATCH --time=2-00:00:00
+#SBATCH --time=5-00:00:00
 #SBATCH --mail-user=araje002@ucr.edu
 #SBATCH --mail-type=ALL
 #SBATCH -p highmem
 #SBATCH -o ../history/5_ScaffMatch-%A.out
-set -eu
+set -e
 
 module load bowtie2/2.3.4.1
+module swap miniconda3 miniconda2
 conda activate DaturaPy2
 
 /rhome/arajewski/bigdata/Datura/software/ScaffMatch/scaffmatch \
   -m \
-  -w Dstr_v1.3_k101-K22 \
-  -c /rhome/arajewski/bigdata/Datura/2_ABySS/k_101/Dstr_v1.3_k101-scaffolds.fa.2kb.fa \
-  -s  193,396,598,801,1003,1205,1400,1600,1800,2000\
-  -i  1931,3955,5983,8009,10032,12053,14111,16141,18172,20000 \
-  -p fr,fr,fr,fr,fr,fr,fr,fr,fr,fr \
-  -1 ultra_ont_raw.I2000.FastSG_K22.sam.fwd.sam,ultra_ont_raw.I4000.FastSG_K22.sam.fwd.sam,ultra_ont_raw.I6000.FastSG_K22.sam.fwd.sam,ultra_ont_raw.I8000.FastSG_K22.sam.fwd.sam,ultra_ont_raw.I10000.FastSG_K22.sam.fwd.sam,ultra_ont_raw.I12000.FastSG_K22.sam.fwd.sam,ultra_ont_raw.I14000.FastSG_K22.sam.fwd.sam,ultra_ont_raw.I16000.FastSG_K22.sam.fwd.sam,ultra_ont_raw.I18000.FastSG_K22.sam.fwd.sam,ultra_ont_raw.I20000.FastSG_K22.sam.fwd.sam \
-  -2 ultra_ont_raw.I2000.FastSG_K22.sam.rev.sam,ultra_ont_raw.I4000.FastSG_K22.sam.rev.sam,ultra_ont_raw.I6000.FastSG_K22.sam.rev.sam,ultra_ont_raw.I8000.FastSG_K22.sam.rev.sam,ultra_ont_raw.I10000.FastSG_K22.sam.rev.sam,ultra_ont_raw.I12000.FastSG_K22.sam.rev.sam,ultra_ont_raw.I14000.FastSG_K22.sam.rev.sam,ultra_ont_raw.I16000.FastSG_K22.sam.rev.sam,ultra_ont_raw.I18000.FastSG_K22.sam.rev.sam,ultra_ont_raw.I20000.FastSG_K22.sam.rev.sam
+  -w Dstr_v1.3_k101-K22_z100 \
+  -c /rhome/arajewski/bigdata/Datura/2_ABySS/k_101/Dstr_v1.3_k101-scaffolds.fa \
+  -s 41,92,193,396,599,802,1000,1208,1400,1600,1800,2000,3000,4000,5000,6000,7000,8000,10000,12000,15000,18000 \
+  -i 414,921,1934,3961,5990,8019,10042,12067,14000,16000,18000,20000,30000,40000,50000,60000,70000,80000,100000,120000,150000,180000 \
+  -p fr,fr,fr,fr,fr,fr,fr,fr,fr,fr,fr,fr,fr,fr,fr,fr,fr,fr,fr,fr,fr,fr \
+  -1 ultra_ont_raw.I500.FastSG_K22.sam.fwd.sam,ultra_ont_raw.I1000.FastSG_K22.sam.fwd.sam,ultra_ont_raw.I2000.FastSG_K22.sam.fwd.sam,ultra_ont_raw.I4000.FastSG_K22.sam.fwd.sam,ultra_ont_raw.I6000.FastSG_K22.sam.fwd.sam,ultra_ont_raw.I8000.FastSG_K22.sam.fwd.sam,ultra_ont_raw.I10000.FastSG_K22.sam.fwd.sam,ultra_ont_raw.I12000.FastSG_K22.sam.fwd.sam,ultra_ont_raw.I14000.FastSG_K22.sam.fwd.sam,ultra_ont_raw.I16000.FastSG_K22.sam.fwd.sam,ultra_ont_raw.I18000.FastSG_K22.sam.fwd.sam,ultra_ont_raw.I20000.FastSG_K22.sam.fwd.sam,ultra_ont_raw.I30000.FastSG_K22.sam.fwd.sam,ultra_ont_raw.I40000.FastSG_K22.sam.fwd.sam,ultra_ont_raw.I50000.FastSG_K22.sam.fwd.sam,ultra_ont_raw.I60000.FastSG_K22.sam.fwd.sam,ultra_ont_raw.I70000.FastSG_K22.sam.fwd.sam,ultra_ont_raw.I80000.FastSG_K22.sam.fwd.sam,ultra_ont_raw.I100000.FastSG_K22.sam.fwd.sam,ultra_ont_raw.I120000.FastSG_K22.sam.fwd.sam,ultra_ont_raw.I150000.FastSG_K22.sam.fwd.sam,ultra_ont_raw.I180000.FastSG_K22.sam.fwd.sam \
+  -2 ultra_ont_raw.I500.FastSG_K22.sam.rev.sam,ultra_ont_raw.I1000.FastSG_K22.sam.rev.sam,ultra_ont_raw.I2000.FastSG_K22.sam.rev.sam,ultra_ont_raw.I4000.FastSG_K22.sam.rev.sam,ultra_ont_raw.I6000.FastSG_K22.sam.rev.sam,ultra_ont_raw.I8000.FastSG_K22.sam.rev.sam,ultra_ont_raw.I10000.FastSG_K22.sam.rev.sam,ultra_ont_raw.I12000.FastSG_K22.sam.rev.sam,ultra_ont_raw.I14000.FastSG_K22.sam.rev.sam,ultra_ont_raw.I16000.FastSG_K22.sam.rev.sam,ultra_ont_raw.I18000.FastSG_K22.sam.rev.sam,ultra_ont_raw.I20000.FastSG_K22.sam.rev.sam,ultra_ont_raw.I30000.FastSG_K22.sam.rev.sam,ultra_ont_raw.I40000.FastSG_K22.sam.rev.sam,ultra_ont_raw.I50000.FastSG_K22.sam.rev.sam,ultra_ont_raw.I60000.FastSG_K22.sam.rev.sam,ultra_ont_raw.I70000.FastSG_K22.sam.rev.sam,ultra_ont_raw.I80000.FastSG_K22.sam.rev.sam,ultra_ont_raw.I100000.FastSG_K22.sam.rev.sam,ultra_ont_raw.I120000.FastSG_K22.sam.rev.sam,ultra_ont_raw.I150000.FastSG_K22.sam.rev.sam,ultra_ont_raw.I180000.FastSG_K22.sam.rev.sam
 
 
 
