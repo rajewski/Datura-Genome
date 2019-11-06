be70ffbb101e6ca98c00cdebcf2cda2ca44d960f
rajewski <rajewski23@gmail.com>
2019-11-06 14:43:30 -0800

    add more files

--- 3_BCGSC/1_LINKS_z100.sh
+++ 3_BCGSC/1_LINKS_z100.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
-#SBATCH --mem=300G
+#SBATCH --mem=270G
 #SBATCH --time=4-00:00:00
 #SBATCH --mail-user=araje002@ucr.edu
 #SBATCH --mail-type=ALL
@@ -13,8 +13,8 @@ set -e
 #module load swig
 module load LINKS/1.8.4
 #specify distance parameters
-dist=(10000 15000 20000 30000)
-window=(10 10 10 10)
+dist=(10000 15000 20000 30000 40000 60000 70000 80000 90000 100000)
+window=(10 10 10 10 10 10 10 10 10 10)
 #kmer size is specified by the array job number
 
 #First Round
@@ -33,20 +33,27 @@ else
 fi
 
 #Second through nth round
-# if restarted, supply a bloom filter with -r Dstr_v1.3_z100_links$((i+2))_k$SLURM_ARRAY_TASK_ID.bloom
-for (( i=0 ; i<${#dist[@]}; i++ ))
+i=0
+while [ $i -lt ${#dist[@]} ]
 do
     if [ ! -e Dstr_v1.3_z100_links$((i+2))_k$SLURM_ARRAY_TASK_ID.scaffolds.fa ]; then
-	LINKS \
-	    -f Dstr_v1.3_z100_links$((i+1))_k$SLURM_ARRAY_TASK_ID.scaffolds.fa \
-	    -s nanopore.txt \
-	    -b Dstr_v1.3_z100_links$((i+2))_k$SLURM_ARRAY_TASK_ID \
-	    -v 1 \
-	    -t ${window[i]} \
-	    -d ${dist[i]} \
-	    -z 100 \
-	    -k $SLURM_ARRAY_TASK_ID
+        echo $(date): Running LINKS to create Dstr_v1.3_z100_links$((i+2))_k$SLURM_ARRAY_TASK_ID.scaffolds.fa
+        LINKS \
+            -f Dstr_v1.3_z100_links$((i+1))_k$SLURM_ARRAY_TASK_ID.scaffolds.fa \
+            -s nanopore.txt \
+            -b Dstr_v1.3_z100_links$((i+2))_k$SLURM_ARRAY_TASK_ID \
+            -v 1 \
+            -t ${window[i]} \
+            -d ${dist[i]} \
+            -k $SLURM_ARRAY_TASK_ID
+        echo $(date): Done.
+        i=$[$i+1]
+    else
+        echo $(date): Dstr_v1.3_z100_links$((i+2))_k$SLURM_ARRAY_TASK_ID.scaffolds.fa found, skipping to next iteration.
+        i=$[$i+1]
     fi
 done
+
+
 
 scontrol show job $SLURM_JOB_ID
