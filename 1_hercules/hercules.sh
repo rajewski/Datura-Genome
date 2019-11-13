ec3ec58c8f72b99131d29a200b6bce391051a5fc
rajewski <rajewski23@gmail.com>
2019-11-13 15:47:53 -0800

    reparameterize LINKS and reset Herc

--- 1_hercules/hercules.sh
+++ 1_hercules/hercules.sh
@@ -2,7 +2,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=20
 #SBATCH --nodes=1
-#SBATCH --mem=200G
+#SBATCH --mem-per-cpu=10G
 #SBATCH --time=6-00:00:00
 #SBATCH --mail-user=araje002@ucr.edu
 #SBATCH --mail-type=ALL
@@ -10,57 +10,76 @@
 set -eu
  
 module load hercules/2017-Nov
-#hercules -1 \
-#    -li /rhome/arajewski/bigdata/Datura/1_QCQA/Dstr.filt_q10_l500_crop50.fastq \
-#    -si /rhome/arajewski/bigdata/Datura/1_QCQA/DstrTrim_1P.fq \
-#    -si /rhome/arajewski/bigdata/Datura/1_QCQA/DstrTrim_2P.fq \
-#    -o preprocessing/
+module load samtools/1.9
+NANOPORE=/rhome/arajewski/bigdata/Datura/1_QCQA/Dstr.filt_q10_l500_crop50.fastq
+
+if [ ! -e preprocessing/compressed_short.fasta ]; then
+    echo $(date): Preprocessing with hercules...
+    hercules -1 \
+	-li $NANOPORE \
+	-si /rhome/arajewski/bigdata/Datura/1_QCQA/DstrTrim_1P.fq \
+	-si /rhome/arajewski/bigdata/Datura/1_QCQA/DstrTrim_2P.fq \
+	-o preprocessing/
+    echo $(date): Done.
+fi
 
 #map illumina to nanopore
-if [ ! -e DstrAlignment.sam ]; then
+COMPNANOPORE=preprocessing/compressed_Dstr.filt_q10_l500_crop50.fastq
+if [ ! -e Dstr.CompNanopore.mmi ]; then
     module load minimap2
-    echo $(date): generating minimap2 index 
+    echo $(date): Generating minimap2 index...
     minimap2 \
 	-x sr \
-	-d Dstr.Nanopore.mmi \
-	/rhome/arajewski/bigdata/Datura/1_QCQA/Dstr.filt_q10_l500_crop50.fastq
-    echo $(date): Minimap2 index created.
-    echo $(date): Generating alignment with Minimap2
+	-I 7G \
+	-d Dstr.CompNanopore.mmi \
+	$COMPNANOPORE
+    echo $(date): Done.
+else
+    echo $(date): Minimap2 index already present.
+fi
+
+if [ ! -e DstrCompAlignment.sam ]; then
+    echo $(date): Generating alignment with Minimap2...
     minimap2 \
 	-ax sr \
+	-I 7G \
 	-t $SLURM_CPUS_PER_TASK \
-	Dstr.Nanopore.mmi \
-	/rhome/arajewski/bigdata/Datura/1_QCQA/DstrTrim_1P.fq /rhome/arajewski/bigdata/Datura/1_QCQA/DstrTrim_2P.fq > DstrAlignment.sam
-    echo $(date) Done with minimap2
+	Dstr.CompNanopore.mmi \
+	preprocessing/compressed_short.fasta \
+	-o DstrCompAlignment.sam
+    echo $(date) Done with minimap2.
 else
-    echo $(date): minimap2 output SAM found.
+    echo $(date): Minimap2 output SAM found.
 fi
 
 #Massage output SAM file
-if [ ! -e /scratch/DstrAlignment.bam ]; then
-    echo $(date): Converting, sorting, and deduplicating minimap2 output
+if [ ! -e DstrCompAlignment.bam ]; then
+    echo $(date): Converting and sorting minimap2 output...
     module load samtools/1.9
-    MEM=$((SLURM_MEM_PER_NODE/1024))G
-    samtools view -S -u -b DstrAlignment.sam | samtools sort -l 0 -@ $SLURM_CPUS_PER_TASK -m $MEM | samtools rmdup -S - DstrAlignment.bam
-    mv /scratch/DstrAlignment.bam /rhome/arajewski/bigdata/Datura/1_hercules/DstrAlignment.bam
-    echo $(date): Done
+    samtools view -S -u -b -T $COMPNANOPORE DstrCompAlignment.sam | samtools sort -l 0 -@ $SLURM_CPUS_PER_TASK -m $((SLURM_MEM_PER_CPU/1024))G -o DstrCompAlignment.bam
+    echo $(date): Done.
 else
-    echo $(date): Output SAM already converted.
+    echo $(date): BAM file already present.
 fi
 
-#only here for testing.
-exit o
+if [ ! -e DstrCompAlignmentNoDup.bam ]; then
+    echo $(date): Deduplicating BAM file...
+    module load samtools/1.9
+    samtools rmdup -S DstrCompAlignment.bam DstrCompAlignmentNoDup.bam
+    echo $(date): Done.
+else
+    echo $(date): Deduplication already complete.
+fi
 
 #Actually run hercules
-if [ ! -e Dstr_Nanopore_HerculesCorrected.fasta ]; then
+if [ ! -e Dstr_Nanopore_Hercules_Corrected.fasta ]; then
     echo $(date): Beginning long read correction with hercules
     hercules -2 \
-	-li /rhome/arajewski/bigdata/Datura/1_QCQA/Dstr.filt_q10_l500_crop50.fastq \
-	-ai alignment.bam \
-	-si /rhome/arajewski/bigdata/Datura/1_QCQA/DstrTrim_1P.fq \
-	-si /rhome/arajewski/bigdata/Datura/1_QCQA/DstrTrim_2P.fq \
-	-t $SLURM_NTASKS \
-	-o Dstr_Nanopore_HerculesCorrected.fasta
+	-li $NANOPORE \
+	-ai DstrCompAlignmentNoDup.bam \
+	-si preprocessing/short.fasta \
+	-t $SLURM_CPUS_PER_TASK \
+	-o Dstr_Nanopore_Hercules_Corrected.fasta
     echo $(date): Done
 else
     echo $(date): Hercules correction already completed.
--- 3_BCGSC/1_LINKS.sh
+++ 3_BCGSC/1_LINKS.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
-#SBATCH --mem=350G
-#SBATCH --time=1-00:00:00
+#SBATCH --mem=800G
+#SBATCH --time=2-00:00:00
 #SBATCH --mail-user=araje002@ucr.edu
 #SBATCH --mail-type=ALL
-#SBATCH -o ../history/LINKS_k%a-%A.out
+#SBATCH -o ../history/LINKS-%A.out
 set -e
 #Initially the mem was set to 200GB, increased to 350GB and then down to 270Gb
 #using either nanopore.txt for uncorrected reads or megareads.txt for the masurca corrected reads
@@ -14,45 +14,48 @@ set -e
 #add LINKS to my path
 module load LINKS/1.8.4
 #specify distance parameters
-dist=(5000 10000 15000 20000 30000 40000 60000 70000 80000 90000 100000)
-window=(2 2 2 2 2 2 2 2 2 2 2)
+dist=(250 500 750 1000 5000 10000 15000 20000 30000 40000 60000 70000 80000 90000 100000)
+window=(6 2 2 2 2 2 2 2 2 2 2 2 2 2 2)
+kmer=19
 
 #First Round
-if [ ! -e Dstr_v1.3_l4d2_links1_k15.scaffolds.fa ]; then
+if [ ! -e Dstr_v1.3_links1_k$kmer.scaffolds.fa ]; then
+    i=0
     LINKS \
 	-f Dstr_v1.3_k101-scaffolds.fa \
 	-s megareads.txt \
-	-b Dstr_v1.3_l4d2_links1_k15 \
+	-b Dstr_v1.3_links1_k$kmer \
 	-v 1 \
-	-t 2 \
-	-d 1000 \
+	-t ${window[i]} \
+	-d ${dist[i]} \
 	-l 4 \
 	-z 100 \
-	-k 15
+	-k $kmer \
+	-r Dstr_v1.3_links1_k$kmer.bloom
 else
     echo $(date): Round 1 of links already complete
+    i=1
 fi
 
 #Second through nth round
-i=0
 while [ $i -lt ${#dist[@]} ]
 do
-    if [ ! -e Dstr_v1.3_l4d2_links$((i+2))_k15.scaffolds.fa ]; then
-	echo $(date): Running LINKS to create Dstr_v1.3_l4d2_links$((i+2))_k15.scaffolds.fa
+    if [ ! -e Dstr_v1.3_links$((i+1))_k$kmer.scaffolds.fa ]; then
+	echo $(date): Running LINKS to create Dstr_v1.3_links$((i+1))_k$kmer.scaffolds.fa
 	LINKS \
-	    -f Dstr_v1.3_l4d2_links$((i+1))_k15.scaffolds.fa \
+	    -f Dstr_v1.3_l4t2_links${i}_k$kmer.scaffolds.fa \
 	    -s megareads.txt \
-	    -b Dstr_v1.3_l4d2_links$((i+2))_k15 \
+	    -b Dstr_v1.3_l4t2_links$((i+1))_k$kmer \
 	    -v 1 \
 	    -t ${window[i]} \
 	    -d ${dist[i]} \
 	    -l 4 \
 	    -z 100 \
-	    -k 15
+	    -k $kmer
 	echo $(date): Done.
 	i=$[$i+1]
     else
-	echo $(date): Dstr_v1.3_l4d2_links$((i+2))_k15.scaffolds.fa found, skipping to next iteration.
+	echo $(date): Dstr_v1.3_l4t2_links$((i+1))_k$kmer.scaffolds.fa found, skipping to next iteration.
 	i=$[$i+1]
     fi
 done
