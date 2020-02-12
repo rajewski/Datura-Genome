9699ddc6dfe029c50a715f4cb27101ff78952242
rajewski <rajewski23@gmail.com>
2020-02-11 22:28:14 -0800

    colorspace to basespace

--- 5_Funannotate/2_Train.sh
+++ 5_Funannotate/2_Train.sh
@@ -9,55 +9,62 @@
 #SBATCH -o ../history/FA_Train-%A.out
 set -e
 
-module load funannotate/git-live
 ASSEMPATH=/rhome/arajewski/bigdata/Datura/3_BCGSC
+RNAPATH=/rhome/arajewski/bigdata/Datura/RNA-seq
 BASE=Dstr_v1.4
 
 #download the publically available RNA seq data
-if [ ! -e ../RNA-seq/ERR2040631_1.fastq ]; then
+if [ ! -e $RNAPATH/ERR2040631_1.fastq.gz ]; then
     echo Downloading ERR2040631 data from SRA...
-    cd /rhome/arajewski/bigdata/Datura/RNA-seq
+    cd $RNAPATH
     module load sratoolkit/2.10.0
-    prefetch ERR2040631
-    fastq-dump --defline-seq '@$sn[_$rn]/$ri' --split-files ERR2040631/
-    cd /rhome/arajewski/bigdata/Datura/5_Funannotate
+    if [ ! -d ERR2040631 ]; then
+	prefetch ERR2040631
+    fi
+    fastq-dump --defline-seq '@$sn[_$rn]/$ri' --split-files -B --gzip ERR2040631/
+    cd $SLURM_SUBMIT_DIR
     echo Done.
 else
     echo ERR2040631 data from SRA already present.
 fi
 
-if [ ! -e ../RNA-seq/SRR9888534_1.fastq ]; then
+if [ ! -e $RNAPATH/SRR9888534_1.fastq.gz ]; then
     echo Downloading SRR9888534 data from SRA...
-    cd /rhome/arajewski/bigdata/Datura/RNA-seq
+    cd $RNAPATH
     module load sratoolkit/2.10.0
-    prefetch SRR9888534
-    fastq-dump --defline-seq '@$sn[_$rn]/$ri' --split-files SRR9888534/
-    cd /rhome/arajewski/bigdata/Datura/5_Funannotate
+    if [ ! -d SRR9888534 ]; then
+	prefetch SRR9888534
+    fi
+    fastq-dump --defline-seq '@$sn[_$rn]/$ri' --split-files -B --gzip SRR9888534/
+    cd $SLURM_SUBMIT_DIR
     echo Done.
 else
     echo SRR9888534 data from SRA already present.
 fi
 
-if [ ! -e ../RNA-seq/s_2_1_DATST_plant_1.fastq.gz ]; then
+if [ ! -e $RNAPATH/s_2_1_DATST_plant_1.fastq.gz ]; then
     echo Downloading MedPlantRNAseq data...
-    cd /rhome/arajewski/bigdata/Datura/RNA-seq
+    cd $RNAPATH
     curl https://medplantrnaseq.org/assemblies/Datura_stramonium.tar.gz
     tar xf Datura_stramonium.tar.gz
     mv Datura_stramonium/s_2_1_DATST_plant_1.txt.gz ./s_2_1_DATST_plant_1.fastq.gz
     mv Datura_stramonium/s_2_2_DATST_plant_1.txt.gz ./s_2_2_DATST_plant_1.fastq.gz
+    #I think you may need to load BBMap's reformat.sh to convert the ASCII of the phred scores
     rm -Rf Datura_stramonium
-    cd cd /rhome/arajewski/bigdata/Datura/5_Funannotate
+    cd $SLURM_SUBMIT_DIR
     echo Done.
 else
     echo MedPlantsRNAseq data already present.
 fi
 
+
 #actually train the predictor
+module load funannotate/1.6.0
 funannotate train \
     -i $BASE.masked.fa \
     -o train \
-    -l /rhome/arajewski/bigdata/Datura/RNA-seq/SRR9888534_1.fastq /rhome/arajewski/bigdata/Datura/RNA-seq/ERR2040631_1.fastq  \
-    -r /rhome/arajewski/bigdata/Datura/RNA-seq/SRR9888534_2.fastq /rhome/arajewski/bigdata/Datura/RNA-seq/ERR2040631_2.fastq  \
+    -l $RNAPATH/SRR9888534_1.fastq.gz $RNAPATH/ERR2040631_1.fastq.gz \
+    -r $RNAPATH/SRR9888534_2.fastq.gz $RNAPATH/ERR2040631_2.fastq.gz \
     --max_intronlen 6000 \
     --species "Datura stramonium" \
     --cpus $SLURM_CPUS_PER_TASK \
