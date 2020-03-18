38b8f9e4c371a24f967c5d2e6d2e7e8ff932422b
rajewski <rajewski23@gmail.com>
2020-03-18 12:38:34 -0700

    archive and clean suboptimal scripts

--- 5_Funannotate/2_Train.sh
+++ 5_Funannotate/2_Train.sh
@@ -3,13 +3,13 @@
 #SBATCH --cpus-per-task=60
 #SBATCH --mem-per-cpu=7G
 #SBATCH --nodes=1
-#SBATCH --time=15:00:00
+#SBATCH --time=5-00:00:00
 #SBATCH --mail-user=araje002@ucr.edu
 #SBATCH --mail-type=ALL
 #SBATCH -o ../history/FA_Train-%A.out
 set -e
 
-ASSEMPATH=/rhome/arajewski/bigdata/Datura/3_BCGSC
+ASSEMPATH=/rhome/arajewski/bigdata/Datura/5_Funannotate
 RNAPATH=/rhome/arajewski/bigdata/Datura/RNA-seq
 BASE=Dstr_v1.4
 
@@ -20,12 +20,12 @@ PASAHOME=/opt/linux/centos/7.x/x86_64/pkgs/PASA/2.3.3/
 #I am going to try retraining the models with only the D. stramonium RNA seq data in the hopes that this reduces the number of gene models prediced
 
 funannotate train \
-    -i $BASE.masked.fa \
-    -o DstrOnly \
+    -i $BASE.masked.l5000.fa \
+    -o trainl5000 \
     -l $RNAPATH/SRR9888534_Trimmed_1P.fastq.gz $RNAPATH/MedPlantTrimmed_1P.fastq.gz \
     -r $RNAPATH/SRR9888534_Trimmed_2P.fastq.gz $RNAPATH/MedPlantTrimmed_2P.fastq.gz \
     --no_trimmomatic \
     --no_normalize_reads \
     --max_intronlen 6000 \
-    --species "Datura stramonium" \
+    --species "Datura stramonium 3" \
     --cpus $SLURM_CPUS_PER_TASK 
