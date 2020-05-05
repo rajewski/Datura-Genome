bc9a10c48220d1c310a4dfafe0c458d3095131fe
rajewski <rajewski23@gmail.com>
2020-05-05 15:39:45 -0700

    archive Fast-plast

--- 2_MaSuRCA333/environment.sh
+++
@@ -1,8 +0,0 @@
-PATH="/rhome/arajewski/.conda/envs/Datura/bin/../CA8/Linux-amd64/bin:/rhome/arajewski/.conda/envs/Datura/bin:/opt/linux/centos/7.x/x86_64/pkgs/vim/8.1/bin:/opt/linux/centos/7.x/x86_64/pkgs/iigb_utilities/1/bin:/opt/linux/centos/7.x/x86_64/pkgs/perl/5.20.2/bin:/opt/linux/centos/7.x/x86_64/pkgs/miniconda2/4.4.10/bin:/opt/linux/centos/7.x/x86_64/pkgs/hdf5/1.10.1/bin:/opt/linux/centos/7.x/x86_64/pkgs/gdal/2.1.3/bin:/opt/linux/centos/7.x/x86_64/pkgs/geos/3.7.1/bin:/opt/linux/centos/7.x/x86_64/pkgs/java/jdk1.8.0_45/bin:/opt/linux/centos/7.x/x86_64/pkgs/netcdf/4.4.1.1/bin:/opt/linux/centos/7.x/x86_64/pkgs/pandoc/2.0/bin:/opt/linux/centos/7.x/x86_64/pkgs/texlive/2017/bin/x86_64-linux:/opt/linux/centos/7.x/x86_64/pkgs/ggobi/2.1.11/bin:/opt/linux/centos/7.x/x86_64/pkgs/R/3.6.0/bin:/opt/linux/centos/7.x/x86_64/pkgs/openmpi/4.0.1-slurm-19.05.0/bin:/opt/linux/centos/7.x/x86_64/pkgs/slurm/19.05.0/bin:/opt/linux/centos/7.x/x86_64/pkgs/slurm/19.05.0/sbin:/rhome/arajewski/.conda/envs/Datura/bin:/opt/linux/centos/7.x/x86_64/pkgs/miniconda3/4.3.31/condabin:/usr/bin:/bin:/usr/local/games:/usr/games:/usr/local/bin:/usr/lpp/mmfs/bin:/opt/ibutils/bin"
-PERL5LIB="/rhome/arajewski/.conda/envs/Datura/bin/../lib/perl:/opt/linux/centos/7.x/x86_64/pkgs/perl/5.20.2/lib/perl5/x86_64-linux/auto:/opt/linux/centos/7.x/x86_64/pkgs/perl/5.20.2/lib/perl5/x86_64-linux:/opt/linux/centos/7.x/x86_64/pkgs/perl/5.20.2/lib/perl5:/opt/linux/centos/7.x/x86_64/pkgs/perl/5.20.2/lib/5.20.2"
-NUM_THREADS="32"
-PE_AVG_READ_LENGTH="124"
-KMER="51"
-MIN_Q_CHAR="33"
-JF_SIZE="200000000"
-ESTIMATED_GENOME_SIZE="1743399272"
