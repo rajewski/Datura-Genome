c5963910c771289ceb797f18ff6f649a43ba3220
rajewski <rajewski23@gmail.com>
2020-07-16 12:00:10 -0700

    start TE analysis with GFF

--- 4_EDTA/1_EDTA.sh
+++ 4_EDTA/1_EDTA.sh
@@ -18,18 +18,14 @@ module load RepeatMasker/4-0-6
 module unload perl
 
 # Link in the genome and CDS for convenience
-ln -sf /bigdata/littlab/arajewski/Datura/5_Funannotate/Dstr_v1.7_Annotation/Dstr_v1.7.sorted.fa Dstr_v1.7.fa
-
-# Trim the long scaffold names if necessary
-#sed 's/\s.*$//' Dstr_v1.5.fa > Dstr_v1.5..trim.fa
-
+#ln -sf /bigdata/littlab/arajewski/Datura/5_Funannotate/Dstr_v1.7_Annotation/Dstr_v1.7.sorted.fa Dstr_v1.7.fa
 # Link in the CDS fasta
-#ln -sf /bigdata/littlab/arajewski/Datura/5_Funannotate/20200426_Dstr_v1.5/predict_results/Datura_stramonium.cds-transcripts.fa Dstr_v1.5.cds.fa
+#ln -sf /bigdata/littlab/arajewski/Datura/5_Funannotate/Dstr_v1.7_Annotation/predict_results/Datura_stramonium.cds-transcripts.fa Dstr_v1.7.cds.fa
 
 EDTA.pl \
     --genome Dstr_v1.7.fa \
     --species others \
-    --cds Dstr_v1.5.cds.fa \
+    --cds Dstr_v1.7.cds.fa \
     --anno 1 \
     --sensitive 1 \
     --threads $SLURM_CPUS_PER_TASK
