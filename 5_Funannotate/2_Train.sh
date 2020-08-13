cd9129708f187c8e130a8ed3f23d215fdafd1b1d
rajewski <rajewski23@gmail.com>
2020-08-13 12:02:05 -0700

    update to v1.8 and mysql

--- 5_Funannotate/2_Train.sh
+++ 5_Funannotate/2_Train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=60
+#SBATCH --cpus-per-task=30
 #SBATCH --mem-per-cpu=7G
 #SBATCH --nodes=1
 #SBATCH --time=7-00:00:00
@@ -13,15 +13,25 @@ RNAPATH=/rhome/arajewski/bigdata/Datura/RNA-seq
 ASSEM=Dstr_v1.7_Annotation/Dstr_v1.7.masked.fa
 
 #actually train the predictor
-module load funannotate/1.6.0
+module unload miniconda2
+module unload anaconda3
+module load miniconda3
+module unload perl
+module unload python
+module load funannotate/1.8.0
+source activate funannotate-1.8
+#module load funannotate/1.6.0
 #change PASA directory becuase the flag below doesnt work
 PASAHOME=/opt/linux/centos/7.x/x86_64/pkgs/PASA/2.3.3/
+export PASACONF=/bigdata/littlab/arajewski/Datura/5_Funannotate/Dstr_v1.7_Annotation_largeIntrons/pasa.CONFIG.template
 
 funannotate train \
     -i $ASSEM \
-    -o Dstr_v1.7_Annotation \
+    -o Dstr_v1.7_Annotation_largeIntrons \
     -l $RNAPATH/SRR9888534_Trimmed_1P.fastq.gz $RNAPATH/MedPlantTrimmed_1P.fastq.gz $RNAPATH/WT_1_R1_val_1.fq.gz $RNAPATH/WT_2_R1_val_1.fq.gz $RNAPATH/WT_3_R1_val_1.fq.gz \
     -r $RNAPATH/SRR9888534_Trimmed_2P.fastq.gz $RNAPATH/MedPlantTrimmed_2P.fastq.gz $RNAPATH/WT_1_R2_val_2.fq.gz $RNAPATH/WT_1_R2_val_2.fq.gz $RNAPATH/WT_1_R2_val_2.fq.gz \
     --no_trimmomatic \
+    --pasa_db mysql \
+    --max_intronlen 7000 \
     --species "Datura stramonium" \
     --cpus $SLURM_CPUS_PER_TASK 
--- 5_Funannotate/3_Predict.sh
+++ 5_Funannotate/3_Predict.sh
@@ -1,16 +1,24 @@
 #!/bin/bash -l
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=60
+#SBATCH --cpus-per-task=30
 #SBATCH --mem-per-cpu=7G
 #SBATCH --time 5-00:00:00
 #SBATCH --mail-user=araje002@ucr.edu
 #SBATCH --mail-type=ALL
 #SBATCH -o ../history/FA_Predict-%A.out
 set -e
-ASSEM=Dstr_v1.7_Annotation_2/Dstr_v1.7.masked.fa
+ASSEM=Dstr_v1.7_Annotation/Dstr_v1.7.masked.fa
 echo Running Funannotate predict on $ASSEM
 
-module load funannotate/1.6.0
+module unload perl
+module unload python
+module unload miniconda2
+module unload anaconda3
+module load miniconda2
+module load funannotate/1.8.0
+source activate funannotate-1.8
+which diamond
+which perl
 module unload CodingQuarry #unload the module or set the EVM weight to 0. it takes five-ever to complete!
 
 #Set AUGUSTUS paths to the local copy
@@ -18,19 +26,20 @@ echo Setting AUGUSTUS path to local directories
 AUGUSTUS_SCRIPTS_PATH=$(realpath ./augustus/scripts )
 AUGUSTUS_BIN_PATH=$(realpath ./augustus/bin )
 AUGUSTUS_CONFIG_PATH=$(realpath ./augustus/config )
+export FUNANNOTATE_DB=/opt/linux/centos/7.x/x86_64/pkgs/funannotate/1.8.0/DB
 echo $AUGUSTUS_SCRIPTS_PATH is the scripts path
 echo $AUGUSTUS_BIN_PATH is the binaries path
 echo $AUGUSTUS_CONFIG_PATH is the config path
+echo $FUNANNOTATE_DB is the database path
 
 funannotate predict \
     -i $ASSEM \
     -s "Datura stramonium" \
-    -o Dstr_v1.7_Annotation_2 \
+    -o Dstr_v1.7_Annotation_largeIntrons \
     --name HAX54_ \
     --organism other \
-    --genemark_mode ES \
+    --genemark_mode ET \
     --repeats2evm \
-    --optimize_augustus \
     --weights augustus:1 hiq:6 genemark:1 pasa:10 codingquarry:0 snap:0 glimmerhmm:0 proteins:1 transcripts:1 \
     --busco_seed_species tomato \
     --busco_db embryophyta_odb9 \
