00a05e78abfd3a4aaa47633aae5c3e287e58adfe
rajewski <rajewski23@gmail.com>
2020-08-24 15:32:03 -0700

    test relocate with only large scaffolds

---
+++ 6_RelocaTE2/0_pBLAT.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -l
+#SBATCH --cpus-per-task=30
+#SBATCH --nodes=1
+#SBATCH --mem-per-cpu=7G
+##SBATCH --time=7-00:00:00
+#SBATCH --mail-user=araje002@ucr.edu
+#SBATCH --mail-type=ALL
+#SBATCH -o ../history/RelocaTE2-%A_%a.out
+set -e
+
+# Create the conda environment if necessary
+# conda create --name pblatEnv -c bioconda pblat
+conda activate pblatEnv
+
+
+
+GFP=(GFP_1 GFP_2 GFP_3 )
+echo $(date): Running RelocaTE2 on ${GFP[$SLURM_ARRAY_TASK_ID]}...
+relocaTE2.py \
+    --genome_fasta ../5_Funannotate/Dstr_v1.7_Annotation/update_results/Datura_stramonium.scaffolds.fa \
+    --reference_ins ../4_EDTA/Dstr_v1.7.fa.mod.EDTA.anno/Dstr_v1.7.fa.mod.EDTA.RM.out \
+    --te_fasta ../4_EDTA/Dstr_v1.7.fa.mod.EDTA.TElib.cleaned.fa \
+    --cpu $SLURM_CPUS_PER_TASK \
+    --size 500 \
+    --fq_dir  ../DNA-seq/${GFP[$SLURM_ARRAY_TASK_ID]}_Trim \
+    --sample ${GFP[$SLURM_ARRAY_TASK_ID]} \
+    --outdir ${GFP[$SLURM_ARRAY_TASK_ID]}_RelocaTE2
